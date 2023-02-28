9973b7152adfce745ccd433bd6b518276e6995a7
casandrabenpor <casandra211@gmail.com>
2023-02-28 16:40:51 +0100

    Trabajo final oxygen shop Casandra1

--- js/nav.js
+++ js/nav.js
@@ -1,25 +1,8 @@
 
 
-
-
-//función acepta una ID como parámetro y verifica si el navegador puede encontrar un elemento con esa ID.
-function muestra_oculta(id) {
-    if (document.getElementById) {
-      let el = document.querySelector('#' + id);
-      //La propiedad de estilo 'display' tiene dos valores posibles: 'block' (para mostrarlo), 'none' (para ocultarlo)
-      el.style.display = (el.style.display == 'none') ? 'block' : 'none';
-    }
-  }
-  
-  window.onload = function () {
-    muestra_oculta('navbarNav');
-  }
-
-// const btnMenu = document.querySelector("your-selector"); // obtenerá el botón 
-// const menu =  document.querySelector("#navbarNav"); // obtendrá el elemento de menú
-
-// btnMenu.addEventListener("click", function(){
-//     menu.classList.toggle("mostrar");
-// });
-
+const burguer = document.getElementById("burguer-button");
+const menu = document.getElementsByClassName("menu");
+burguer.addEventListener("click", () => {
+ menu[0].classList.toggle("menu-visible");
+});
   
--- js/obj.js
+++ js/obj.js
@@ -1,4 +1,3 @@
-
 const autores = [
     {
         nombre: "Juan",
